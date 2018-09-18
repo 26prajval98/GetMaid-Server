40f73b2398b68bb685be05cf4a639bcf19df49a9
26prajval98 <26prajval98@gmail.com>
2018-09-18 16:54:28 +0530

    Path added

--- src/GetMaid/handlers/index.go
+++ src/GetMaid/handlers/index.go
@@ -9,14 +9,24 @@ import (
 
 var wg sync.WaitGroup
 
+var path = "/"
+
+var RQ *http.Request
+
 func getHandler(res http.ResponseWriter) {
 	io.WriteString(res, "Welcome to GetMaid")
 	wg.Done()
 }
 
+func checkCase(method string) (y bool) {
+	y = method == RQ.Method && path == RQ.URL.Path
+	return
+}
+
 func IndexHandler(res http.ResponseWriter, req *http.Request) error {
-	switch req.Method {
-	case "GET":
+	RQ = req
+	switch {
+	case checkCase("GET"):
 		wg.Add(1)
 		go getHandler(res)
 	default:
