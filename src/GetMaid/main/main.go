41a8ce561ecad5d596dfc640f9b3cf5667f39011
26prajval98 <26prajval98@gmail.com>
2018-10-13 15:11:12 +0530

    Updated Main

--- src/GetMaid/main/main.go
+++ src/GetMaid/main/main.go
@@ -14,6 +14,6 @@ func main() {
 	server.HandlePath("/", index.Handler)
 	server.HandlePath("/signup", signup.Handler)
 
-	fmt.Println("Starting Server")
+	fmt.Println("Server Started")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
