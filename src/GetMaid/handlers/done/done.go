b0523fea72fcc59f1cb41f08023fbadd311d54a7
26prajval98 <26prajval98@gmail.com>
2018-11-06 13:07:43 +0530

    Almost done

--- src/GetMaid/handlers/done/done.go
+++ src/GetMaid/handlers/done/done.go
@@ -15,8 +15,25 @@ func Handler(res http.ResponseWriter, req *http.Request) error {
 
 	fmt.Println(req.URL.Query())
 
-	_, e := db.Exec("UPDATE services SET DONE = 1 WHERE Service_id=?", id)
+	db.Exec("UPDATE services SET DONE = 1 WHERE Service_id=?", id)
 
-	fmt.Println(e)
+	var maidId string
+
+	x := db.QueryRow("SELECT Maid_id FROM services WHERE Service_id = ?", id)
+
+	x.Scan(&maidId)
+
+	t, err := db.Query("SELECT id FROM maid_online WHERE Maid_id= ?", maidId)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	for t.Next() {
+		var sid string
+		t.Scan(&sid)
+		db.Exec("UPDATE maid_online SET Count = Count - 1 WHERE id=?", sid)
+	}
+
 	return nil
 }
--- src/GetMaid/handlers/maidsearch/get.go
+++ src/GetMaid/handlers/maidsearch/get.go
@@ -6,6 +6,7 @@ import (
 	"GetMaid/handlers/types"
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -35,7 +36,11 @@ func search(req *http.Request, res http.ResponseWriter) {
 	}
 
 	//noinspection SqlResolve
-	CountUpdate, e = db.Prepare(`UPDATE maid_online SET Count=? WHERE Id=?`)
+	CountUpdate, e = db.Prepare(`UPDATE maid_online SET Count=Count+1 WHERE Maid_id=?`)
+
+	if e != nil {
+		fmt.Println(e.Error())
+	}
 
 	//noinspection SqlResolve
 	result, err := db.Query(`SELECT DISTINCT o.Maid_id, o.Count FROM maid s, maid_services x, address a, pincodes p, maid_online o WHERE x.Service_name=? AND s.AddressId=a.id AND ((a.Pincode=p.Pincode1 AND p.Pincode2=?) or a.Pincode = ?) AND x.Maid_id=s.Maid_id AND s.Maid_id=o.Maid_id AND o.Count<2 LIMIT  1`, reqServices, hirerPincode, hirerPincode)
@@ -53,8 +58,7 @@ func search(req *http.Request, res http.ResponseWriter) {
 
 	for result.Next() {
 		err := result.Scan(&id, &ct)
-		ct = ct + 1
-		CountUpdate.Exec(ct, id)
+		CountUpdate.Exec(id)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
